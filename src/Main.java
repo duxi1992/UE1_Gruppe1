bcbb6f8e0e5b929a88b4c182865af37d3eed579c
Riccardo Duxenneuner <riccardo@duxi.at>
2018-10-11 15:20:38 +0200

    Alter wurde hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        int alter = 25;
         String vorname = "Riccardo Duxenneuner";
-        System.out.println("hello " + vorname);
+        System.out.println("hello " + vorname + "du bist " + alter + " Jahre alt");
 
     }
 }
