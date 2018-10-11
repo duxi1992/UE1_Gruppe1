5de1dcd91b7812556f30267567fafa80020beff0
Riccardo Duxenneuner <riccardo@duxi.at>
2018-10-11 15:52:49 +0200

    Jahrgang hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ public class Main {
         int alter = 25;
         int Jahrgang = 2018;
         String vorname = "Riccardo ";
-        String nachname = " Duxenneuner";
-        String Herkunft = " Kufstein, Tirol";
 
-        System.out.println("hello " + vorname + nachname +"du bist " + alter + " Jahre alt und aus" + Herkunft + " und du hast " + Jahrgang + " zu studieren begonnen.");
+
+        System.out.println("hello " + vorname + "du bist " + alter + " Jahre alt und du hast " + Jahrgang + " zu studieren begonnen.");
 
     }
 }
